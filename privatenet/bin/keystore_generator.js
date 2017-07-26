0add898598bb2e64de635a217bea98a7a3029c8b
Stephen Harrington <stephen.thomas.harrington@gmail.com>
2017-07-26 14:22:22 -0400

    file write placed inside callback

--- privatenet/bin/keystore_generator.js
+++ privatenet/bin/keystore_generator.js
@@ -19,25 +19,24 @@ fs.readFile(dir + 'pk_password_strings', 'utf8', function (err, data) {
       return console.log(err);
   }
     pwds = data.toString().split("\n");
+
+    var wallet_password = "this is my super secret wallet password";
+
+    for (idx in pwds) {
+        var privatekey = crypto.createHash('sha256').update(pwds[idx]).digest('hex');
+        var key        = Buffer.from(privatekey,'hex');
+        var wallet     = Wallet.fromPrivateKey(key);
+        var date       = new Date();
+
+        var filename   = 'UTC--' + date.toISOString().replace(/:/g,'-') + '--' +
+            wallet.getAddressString().slice(2)
+
+        fs.writeFile(dir + filename, wallet.toV3String(wallet_password), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+        console.log(filename + " saved!");
+    }
 });
-
-var wallet_password = "this is my super secret wallet password";
-
-for (idx in pwds) {
-    var privatekey = crypto.createHash('sha256').update(pwds[idx]).digest('hex');
-    var key        = Buffer.from(privatekey,'hex');
-    var wallet     = Wallet.fromPrivateKey(key);
-    var date       = new Date();
-    
-    var filename   = 'UTC--' + date.toISOString().replace(/:/g,'-') + '--' +
-        wallet.getAddressString().slice(2)
-    
-    fs.writeFile(dir + filename, wallet.toV3String(wallet_password), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-    console.log(filename + " saved!");
-}
-
 
